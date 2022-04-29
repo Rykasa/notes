34ab28b8e8754fcded8f7cccf370d034ccef4d8e
LAK <email@lak.com>
2022-04-29 18:56:48 -0300

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+const notepadText = document.querySelector('.notepad-text')
+const addBtn = document.querySelector('.add-btn')
+
+addBtn.addEventListener('click', ()=>{
+    console.log('click')
+})
