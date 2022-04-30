cc4b14681e34bab9c32c62988fc800107b8bda52
LAK <email@lak.com>
2022-04-30 15:59:17 -0300

    create add function and showNoteList function

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const notepadText = document.querySelector('.notepad-text')
 const addBtn = document.querySelector('.add-btn')
+const notes = document.querySelector('.notes-wrapper')
+let noteList = []
 
 addBtn.addEventListener('click', ()=>{
-    console.log('click')
+    const noteText = notepadText.value
+    if(JSON.parse(localStorage.getItem('notesCollection')) !== null){
+        noteList = JSON.parse(localStorage.getItem('notesCollection'))
+        const noteHTML = '<div class="note"><div class="note-text">' + noteText + '</div></div>'
+        noteList.push(noteHTML)
+        localStorage.setItem('notesCollection', JSON.stringify(noteList))
+        showNoteList()
+    }else{
+        noteList.push('<div class="note"><div class="note-text">' + noteText + '</div></div>')
+        showNoteList()
+    }
+    notepadText.value = ''
 })
+
+const showNoteList = () =>{
+    if(JSON.parse(localStorage.getItem('notesCollection')) !== null) {
+        notes.innerHTML = JSON.parse(localStorage.getItem('notesCollection')).join('')
+    }else{
+        localStorage.setItem('notesCollection', JSON.stringify(noteList))
+        notes.innerHTML = JSON.parse(localStorage.getItem('notesCollection')).join('')
+    }
+}
+
+showNoteList()
